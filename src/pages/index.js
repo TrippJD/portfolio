3cb27aaa47dd2ac6f66fc50f516aa891ce4f7a33
Tripp <tripp4414@yahoo.com>
2019-07-25 23:43:35 -0600

    form 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,6 +11,11 @@ import Bar from "../components/bar/bar"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
+    <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+      <input type="text" name="name" />
+      <input type="email" name="email" />
+      <textarea name="message"></textarea>
+    </form>
     <Header />
     <Intro />
     <Headline />
