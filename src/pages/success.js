662535d10737bdfec4628b7324ddc74077b8b05e
Tripp <tripp4414@yahoo.com>
2019-07-26 00:18:25 -0600

    form 6

--- src/pages/success.js
+++ src/pages/success.js
@@ -2,14 +2,12 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
-const SecondPage = () => (
+const Success = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Success</h1>
+    <h1 slyle={{ margin: "100px" }}>Success</h1>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
 
-export default SecondPage
+export default Success
