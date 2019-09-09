d6147c1eb2e2c76a7661c098de2e257df5e8de40
Tripp <tripp4414@yahoo.com>
2019-09-08 22:53:33 -0600

    intro banner

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,9 +1,10 @@
 import React from "react"
 import "./intro.css"
+import Banner from "../../images/introbanner.png"
 
 function Intro() {
   return (
-    <div className="intro-div">
+    <div className="intro-div" style={{ backgroundImage: `url(${Banner})` }}>
       <p className="intro-text">
         Hello, I'm Jd Tripp. I'm a freelance web developer and I'd love to take
         your ideas and turn them into something you'll love.
