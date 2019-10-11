1174bfbb04014eb1ddddfa31d1a24c74d492b008
Tripp <tripp4414@yahoo.com>
2019-10-11 01:12:39 -0600

    Light Header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,33 +1,10 @@
 import React from "react"
 import "./header.css"
-import TextLoop from "react-text-loop"
-
-import HeaderImage from "../../images/banner.jpg"
-// import HeaderImage from "../../images/stars-header.jpg"
 
 function Header() {
   return (
     <header
-      style={{
-        backgroundImage: `url(${HeaderImage})`,
-      }}
       id="header">
-      <div className="headerTextDiv">
-        <h2 id="headerText">
-          <TextLoop
-            children={["Take your dreams", "and ideas", ""]}
-            interval={[3000, 2000, 3000 * 500]}
-          />
-          <TextLoop
-            children={["", "To the stars", ""]}
-            interval={[5000, 3000, 3000 * 500]}
-          />
-          <TextLoop
-            children={["", "With Amazing Design"]}
-            interval={[9000, 3000 * 500]}
-          />
-        </h2>
-      </div>
     </header>
   )
 }
