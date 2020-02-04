6f7915d6eaeef7f6a40f8bc2c009f8b3ebc1cd91
unknown <tripp4414@gmail.com>
2020-02-03 21:23:26 -0700

    name

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -6,8 +6,8 @@ function Intro() {
   return (
     <div className="intro-div" style={{ backgroundImage: `url(${Banner})` }}>
       <p className="intro-text">
-        Hello, I'm Jd Tripp. I'm a freelance web developer and I'd love to take
-        your ideas and turn them into something you'll love.
+        Hello, I'm Josh Tripp. I'm a freelance web developer and I'd love to
+        take your ideas and turn them into something you'll love.
       </p>
     </div>
   )
